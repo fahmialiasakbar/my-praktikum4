be868ee6f7bf4b5e91630886c5604b46bb3abe1e
Fahmi Akbar <fahmialiasakbar@gmail.com>
2024-11-05 18:50:02 +0700

    Latihan

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [isList, setIsList] = useState(false);
+
   useEffect(() => {
     const form = document.getElementById("myForm");
 
@@ -10,28 +12,33 @@ export default function Home() {
       e.preventDefault();
       const name = document.getElementById("name").value;
       const notes = document.getElementById("notes").value;
+      const color = document.getElementById("color").value;
 
-      console.log("Form Submitted:", { name, notes });
+      console.log("Form Submitted:", { name, notes, color });
       var targetElement = document.getElementById("cardlist");
       var newHtml =
-        `<div class="card"
+        `<div class="card `+ color +`"
           <div class="card-header">
             <div class="card-title">` +
               name +
             `</div>
-          </div>
           <div class="card-content">` +
             notes +
           `</div>
         </div>`;
       targetElement.innerHTML += newHtml;
+
+      document.getElementById("name").value = "";
+      document.getElementById("notes").value = "";
+      document.getElementById("color").value = "white";
+
+      setIsList(true);
     };
 }, []);
 
-const isList = true;
 return (
   <main className={styles.main}>
-    <section id="form" className={styles.section} style={{ display: "none" }}>
+    <section id="form" className={styles.section} style={{ display: isList ? 'none' : 'flex' }}>
       <div className={styles.width50p}>
         <h2 className={styles.sectionTitle}>Create Note</h2>
         <form id="myForm" className={styles.form}>
@@ -45,6 +52,16 @@ return (
           </label>
           <textarea className={styles.textarea} id="notes" name="notes"></textarea>
           
+          <label className={styles.label} htmlFor="color">
+            Color:
+          </label>
+          <select className={styles.select} id="color" name="color">
+            <option value="white">White</option>
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="yellow">Yellow</option>
+          </select>
+
           <button className={styles.button} type="submit">
             Tambahkan
           </button>
@@ -55,9 +72,9 @@ return (
     <section
       id="listnote"
       className={styles.section}
-      style={{ display: "block" }}
+      style={{ display: isList ? 'block' : 'none' }}
     >
-      <h2 className={styles.sectionTitle}>List Note </h2>
+      <h2 className={styles.sectionTitle}>List Note</h2>
       <div id="cardlist">
         <div className="card">
           <div className="card-header">
